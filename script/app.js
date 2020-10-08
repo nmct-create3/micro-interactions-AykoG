09d22857f5323e13a3147e460e2c7d770fa83ea5
Ayko Geeraert <ayko.geeraert@student.howest.be>
2020-10-08 11:18:09 +0200

    Update app.js

--- script/app.js
+++ script/app.js
@@ -3,7 +3,18 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
+    let checkbox = document.querySelector(`.c-password-toggle__checkbox`);
+    let passwordInput = document.querySelector(`.c-password-toggle__input`);
+    checkbox.addEventListener(`change`, function() {
+        if (checkbox.checked) {
+            passwordInput.type = "text";
+            console.log("checked")
+        }
+        else {
+            passwordInput.type = "password";
+            console.log("unchecked")
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
